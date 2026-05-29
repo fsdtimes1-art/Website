ba77ee08ad28e006de2a3f8294597b2f54b8396d
Mad-iii <156696189+Mad-iii@users.noreply.github.com>
2026-05-29 17:28:14 +0000

    Tweaking things

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar          from './components/Navbar'
-import Footer          from './components/Footer'
-import Home            from './pages/Home'
-import Events          from './pages/Events'
-import EventDetail     from './pages/EventDetail'
-import PaymentSuccess  from './pages/PaymentSuccess'
-import Portfolio       from './pages/Portfolio'
-import BookMeeting     from './pages/BookMeeting'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect }                  from 'react'
+import Navbar                          from './components/Navbar'
+import Footer                          from './components/Footer'
+import Home                            from './pages/Home'
+import Events                          from './pages/Events'
+import EventDetail                     from './pages/EventDetail'
+import PaymentSuccess                  from './pages/PaymentSuccess'
+import Portfolio                       from './pages/Portfolio'
+import BookMeeting                     from './pages/BookMeeting'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+  }, [pathname])
+
+  return null
+}
 
 export default function App() {
   return (
     <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <ScrollToTop />
       <Navbar />
 
       <main style={{ flex: 1 }}>
